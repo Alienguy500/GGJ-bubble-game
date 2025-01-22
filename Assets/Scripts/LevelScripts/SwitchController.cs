10eab1d203ee9828b8bb3aed4de44873b1617c9d
Alienguy500 <alienguy500@gmail.com>
2025-01-22 14:07:22 +0000

    clunky levers

--- Assets/Scripts/LevelScripts/SwitchController.cs
+++ Assets/Scripts/LevelScripts/SwitchController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class SwitchController : MonoBehaviour
 {
+    Animator animator;
+    AudioSource audioSource;
+    int isOnHash;
+    bool isisOnOn;
+    public bool isOn = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
+        //isOnHash = animator.StringToHash("isOn");
+
+        isisOnOn = false;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool isOn = animator.GetBool("isOn");
+        if (isOn)
+        {
+            if (!isisOnOn)
+            {
+                isisOnOn = true;
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (isisOnOn)
+            {
+                isisOnOn = false;
+                audioSource.Play();
+            }
+        }
+        if ()
+        {
+            animator.SetBool("isOn", Input.GetKeyDown("e"))
+        }
     }
 }
