008e94ea7ed2341d6427f0eaa8ec6bcd1963379a
Alienguy500 <alienguy500@gmail.com>
2025-01-22 10:19:28 +0000

    added piston sound effects

--- Assets/Scripts/SFX/SoundManager.cs
+++ Assets/Scripts/SFX/SoundManager.cs
@@ -6,8 +6,15 @@ using UnityEngine.UI;
 public class SoundManager : MonoBehaviour
 {
     public Slider masterVolumeSlider;
+    public float volume = 0;
+
+    void Start()
+    {
+        volume = masterVolumeSlider.value;
+    }
     public void ChangeMasterVolume()
     {
         AudioListener.volume = masterVolumeSlider.value;
+        volume = masterVolumeSlider.value;
     }
 }
